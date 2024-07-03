97f331e9ce01160e6e829b5cfefd5549ac38b8ed
PabloVD <pablo.villanueva.domingo@gmail.com>
2024-07-03 15:50:33 +0200

    Get lanes affected by traffic lights

--- autopilot_nn_input.py
+++ autopilot_nn_input.py
@@ -9,12 +9,6 @@ from roadgraph import RoadGraph
 import torch
 from controller import VehiclePIDController
 
-outpath = "testframes/"
-
-if not os.path.exists(outpath):
-    os.system("mkdir "+outpath)
-else:
-    os.system("rm "+outpath+"*")
 
 class CustomWaypoint():
     def __init__(self, pos):
@@ -25,230 +19,246 @@ def get_closest_waypoint(pos):
     wp = map.get_waypoint(loc, project_to_road=True, lane_type=(carla.LaneType.Driving))
     return wp
 
-device = "cuda"
 
-# True for using NN, otherwise uses standard traffic manager
-use_nn = True
+def main():
 
-# If True, uses PID to update position, otherwise teleports the vehicle
-use_pid = False
+    outpath = "testframes/"
 
-# True for fixed birdview spectator, otherwise follows an agent
-fixed_spec = True
-
-n_channels = 11
-
-# Timestep, 10Hz as Waymo data (do not modify)
-dt = 0.1
-
-#--- Constants
-num_npcs = 9
-prev_steps = n_channels
-
-# Set simulation
-client = carla.Client()
-#client.load_world("Town03")
-world = client.get_world()
-map = world.get_map()
-blueprint_library = world.get_blueprint_library()
-settings = world.get_settings()
-settings.synchronous_mode = True
-settings.fixed_delta_seconds = dt
-world.apply_settings(settings)
-traffic_manager = client.get_trafficmanager()
-traffic_manager.set_synchronous_mode(True)
-
-# Spawn npcs
-spawn_points = map.get_spawn_points()
-blueprint_list = blueprint_library.filter("*audi*")
-blueprint = blueprint_list[0]
-npcs = []
-for i in range(num_npcs):
-    npc = world.try_spawn_actor(blueprint, spawn_points[i])
-    if not npc:
-        print("npc "+str(i)+" not spawned")
+    if not os.path.exists(outpath):
+        os.system("mkdir "+outpath)
     else:
-        npc.set_autopilot(True)
-        npcs.append( npc )
+        os.system("rm "+outpath+"*")
 
-# Length and width bounding boxes
-bb_npcs = []
-for npc in npcs:
-    bb = npc.bounding_box
-    bb_npcs.append([bb.extent.x*2, bb.extent.y*2])
-bb_npcs = np.array(bb_npcs)
+    device = "cuda"
 
-# Create buffer with previous steps info
-agents_buffer_list = [deque(maxlen=prev_steps) for i in range(len(npcs))]
+    # True for using NN, otherwise uses standard traffic manager
+    use_nn = True
 
-# Road
-roadnet = RoadGraph(world)
-list_roads = roadnet.each_road_waypoints
-# list_wps = []
-# for road in list_roads:
-#     list_wps.extend(road)
-# list_wps = [ [wp.x, wp.y] for wp in list_wps]
+    # If True, uses PID to update position, otherwise teleports the vehicle
+    use_pid = False
 
-# Load model
-model = torch.jit.load("models/model7.pt")
-model = model.to(device)
+    # True for fixed birdview spectator, otherwise follows an agent
+    fixed_spec = True
 
-# for npc in npcs:
-#     npc.destroy()
-# exit()
+    n_channels = 11
 
-spec_offset = carla.Location(z=2,x=-2)
+    # Timestep, 10Hz as Waymo data (do not modify)
+    dt = 0.1
 
-# Set spectator
-if fixed_spec:
-    spectator = world.get_spectator()
-    specloc = carla.Location(z=220)
-    specrot = carla.Rotation(pitch=-90)
-    spectransf = carla.Transform(location=specloc, rotation=specrot)
-    spectator.set_transform(spectransf)
+    #--- Constants
+    num_npcs = 9
+    prev_steps = n_channels
 
-# Set camera
-# camera_bp = blueprint_library.find('sensor.camera.rgb')
-# camera = world.spawn_actor(camera_bp, carla.Transform(carla.Location(x=50)), attach_to=spectator)
-# camera.listen(lambda image: image.save_to_disk('_out/%06d.png' % image.frame))
+    # Set simulation
+    client = carla.Client()
+    #client.load_world("Town03")
+    world = client.get_world()
+    map = world.get_map()
+    blueprint_library = world.get_blueprint_library()
+    settings = world.get_settings()
+    settings.synchronous_mode = True
+    settings.fixed_delta_seconds = dt
+    world.apply_settings(settings)
+    traffic_manager = client.get_trafficmanager()
+    traffic_manager.set_synchronous_mode(True)
 
-# Traffic lights buffer
-traffic_lights = world.get_actors().filter('traffic.traffic_light*')
-tl_buffer_list = [deque(maxlen=prev_steps) for i in range(len(traffic_lights))]
+    # Spawn npcs
+    spawn_points = map.get_spawn_points()
+    blueprint_list = blueprint_library.filter("*audi*")
+    blueprint = blueprint_list[0]
+    npcs = []
+    for i in range(num_npcs):
+        npc = world.try_spawn_actor(blueprint, spawn_points[i])
+        if not npc:
+            print("npc "+str(i)+" not spawned")
+        else:
+            npc.set_autopilot(True)
+            npcs.append( npc )
 
-# PID controllers
-args_lateral_dict = {'K_P': 0., 'K_I': 0.05, 'K_D': 0.2, 'dt': dt}
-args_longitudinal_dict = {'K_P': 1.0, 'K_I': 0.05, 'K_D': 0, 'dt': dt}
-controllers = []
-for npc in npcs:
-    vehicle_controller = VehiclePIDController(npc,
-                                            args_lateral=args_lateral_dict,
-                                            args_longitudinal=args_longitudinal_dict,
-                                            offset=0,
-                                            max_throttle=0.75,
-                                            max_brake=0.5,
-                                            max_steering=0.8)
-    controllers.append(vehicle_controller)
+    # Length and width bounding boxes
+    bb_npcs = []
+    for npc in npcs:
+        bb = npc.bounding_box
+        bb_npcs.append([bb.extent.x*2, bb.extent.y*2])
+    bb_npcs = np.array(bb_npcs)
 
-# Deactivate some layers for debugging
-map_layer_names = [
-            carla.MapLayer.Buildings,
-            carla.MapLayer.Decals,
-            carla.MapLayer.Foliage,
-            carla.MapLayer.ParkedVehicles,
-            carla.MapLayer.Props,
-            carla.MapLayer.StreetLights,
-            carla.MapLayer.Walls,
-        ]
+    # Create buffer with previous steps info
+    agents_buffer_list = [deque(maxlen=prev_steps) for i in range(len(npcs))]
 
-for layer in map_layer_names:
-    world.unload_map_layer(layer)
+    # Road
+    roadnet = RoadGraph(world)
+    list_roads = roadnet.each_road_waypoints
+    # list_wps = []
+    # for road in list_roads:
+    #     list_wps.extend(road)
+    # list_wps = [ [wp.x, wp.y] for wp in list_wps]
 
-min_frame = 100
-run_nn = False
+    # Load model
+    model = torch.jit.load("models/model7.pt")
+    model = model.to(device)
 
-world.tick()
-frame_ind = 0
-
-print("Running with",len(npcs),"agents, total vehicles in simulation:", len(world.get_actors().filter('vehicle.*')))
-
-try:
-    while True:  
-
-        # Attatch spectator to an actor
-        if not fixed_spec:
-            spectator = world.get_spectator()
-            spectransf = npcs[0].get_transform()
-            spectransf = carla.Transform(location=spectransf.location+spec_offset, rotation=spectransf.rotation)
-            spectator.set_transform(spectransf)
-
-        # Get agents information (x, y, yaw)
-        for i, npc in enumerate(npcs):
-
-            transf = npc.get_transform()
-            agents_buffer_list[i].append([transf.location.x, transf.location.y, transf.rotation.yaw])
-
-        # Get traffic light information
-        for j, tl in enumerate(traffic_lights):
-            transf = tl.get_transform()
-            tl_buffer_list[j].append( [tl.get_state(), transf.location.x, transf.location.y] )
-
-        # (N,timesteps,3)
-        agents_arr = np.array(agents_buffer_list)
-
-        if not run_nn and use_nn and agents_arr.shape[1]==n_channels and frame_ind>min_frame:
-            print("Disabling traffic manager, switching to neural traffic")
-            # for npc in npcs:
-            #     npc.set_autopilot(False)
-            #     control = carla.VehicleControl(steer=0., throttle=0., brake=1.0, hand_brake = False, manual_gear_shift = False)
-            #     npc.apply_control(control)
-            run_nn = True
-
-        if run_nn:
-
-            # (N,channels,rastersize,rastersize)
-            raster = rasterize_input(agents_arr, bb_npcs, list_roads)
-            np.save("testframes/test_"+str(frame_ind),raster[0])
-            print(frame_ind, agents_arr.shape, raster.shape)
-            
-            # Run model
-            raster = torch.tensor(raster, device=device, dtype=torch.float32)
-            confidences, logits  = model(raster)
-            # print(confidences.shape, logits.shape)
-
-            #currpos, yaw = agents_arr[:,-1,:2], agents_arr[:,-1,2]
-            
-            # TO DO improve with array multiplication
-            for j in range(len(agents_arr)):
-
-                pred = logits[j,confidences[j].argmax()].detach().cpu().numpy()
-
-                currpos, yaw = agents_arr[j,-1,:2], agents_arr[j,-1,2]*np.pi/180.
-
-                rot_matrix = np.array([
-                    [np.cos(-yaw), -np.sin(-yaw)],
-                    [np.sin(-yaw), np.cos(-yaw)],
-                ])
-            
-                pred = pred@rot_matrix + currpos 
-
-                if use_nn:
-                    
-                    if use_pid:
-                        # Apply PID controller
-
-                        #target_vel = diffpos/dt
-                        target_vel = (pred[1]-currpos)/(2.*dt)
-                        target_speed = np.sqrt( target_vel[0]**2. + target_vel[1]**2. )
-                        next_wp = get_closest_waypoint(pred[0])
-                        #next_wp = CustomWaypoint(pred[0])
-                        control = controllers[j].run_step(target_speed, next_wp)
-                        npcs[j].apply_control(control)
-
-                    else:
-                        # Displace directly the vehicle to the predicted position
-
-                        nextpos =  pred[0]
-
-                        # Estimate orientation
-                        diffpos = nextpos - currpos
-                        newyaw = np.arctan2(diffpos[1], diffpos[0])*180./np.pi
-                        
-                        nextloc = carla.Location(x=nextpos[0], y=nextpos[1])
-                        nextrot = carla.Rotation(yaw=newyaw)
-
-                        npcs[j].set_transform(carla.Transform(location=nextloc, rotation=nextrot))
-
-                np.save("testframes/prev_{:d}_{:03d}".format(j, frame_ind),agents_arr[j,:,:2])
-                np.save("testframes/pred_{:d}_{:03d}".format(j, frame_ind),pred)
-
-        world.tick()
-        frame_ind+=1
-
-finally:
-    vehicles = world.get_actors().filter('vehicle.*')
-    for vehicle in vehicles:
-        vehicle.destroy()
     # for npc in npcs:
     #     npc.destroy()
+    # exit()
+
+    spec_offset = carla.Location(z=2,x=-2)
+
+    # Set spectator
+    if fixed_spec:
+        spectator = world.get_spectator()
+        specloc = carla.Location(z=220)
+        specrot = carla.Rotation(pitch=-90)
+        spectransf = carla.Transform(location=specloc, rotation=specrot)
+        spectator.set_transform(spectransf)
+
+    # Set camera
+    # camera_bp = blueprint_library.find('sensor.camera.rgb')
+    # camera = world.spawn_actor(camera_bp, carla.Transform(carla.Location(x=50)), attach_to=spectator)
+    # camera.listen(lambda image: image.save_to_disk('_out/%06d.png' % image.frame))
+
+    # Traffic lights buffer
+    traffic_lights = world.get_actors().filter('traffic.traffic_light*')
+    #tl_buffer_list = [deque(maxlen=prev_steps) for i in range(len(traffic_lights))]
+    tl_lanes = roadnet.get_tl_lanes(traffic_lights)
+
+    # PID controllers
+    args_lateral_dict = {'K_P': 0., 'K_I': 0.05, 'K_D': 0.2, 'dt': dt}
+    args_longitudinal_dict = {'K_P': 1.0, 'K_I': 0.05, 'K_D': 0, 'dt': dt}
+    controllers = []
+    for npc in npcs:
+        vehicle_controller = VehiclePIDController(npc,
+                                                args_lateral=args_lateral_dict,
+                                                args_longitudinal=args_longitudinal_dict,
+                                                offset=0,
+                                                max_throttle=0.75,
+                                                max_brake=0.5,
+                                                max_steering=0.8)
+        controllers.append(vehicle_controller)
+
+    # Deactivate some layers for debugging
+    map_layer_names = [
+                carla.MapLayer.Buildings,
+                carla.MapLayer.Decals,
+                carla.MapLayer.Foliage,
+                carla.MapLayer.ParkedVehicles,
+                carla.MapLayer.Props,
+                carla.MapLayer.StreetLights,
+                carla.MapLayer.Walls,
+            ]
+
+    for layer in map_layer_names:
+        world.unload_map_layer(layer)
+
+    min_frame = 100
+    run_nn = False
+
+    world.tick()
+    frame_ind = 0
+
+    print("Running with",len(npcs),"agents, total vehicles in simulation:", len(world.get_actors().filter('vehicle.*')))
+
+    try:
+        while True:  
+
+            # Attatch spectator to an actor
+            if not fixed_spec:
+                spectator = world.get_spectator()
+                spectransf = npcs[0].get_transform()
+                spectransf = carla.Transform(location=spectransf.location+spec_offset, rotation=spectransf.rotation)
+                spectator.set_transform(spectransf)
+
+            # Get agents information (x, y, yaw)
+            for i, npc in enumerate(npcs):
+
+                transf = npc.get_transform()
+                agents_buffer_list[i].append([transf.location.x, transf.location.y, transf.rotation.yaw])
+
+            # Get traffic light information
+            # for j, tl in enumerate(traffic_lights):
+            #     transf = tl.get_transform()
+            #     tl_buffer_list[j].append( [tl.get_state(), transf.location.x, transf.location.y] )
+            tl_cols = [ tl.get_state() for tl in traffic_lights ]
+
+            # (N,timesteps,3)
+            agents_arr = np.array(agents_buffer_list)
+
+            if not run_nn and use_nn and agents_arr.shape[1]==n_channels and frame_ind>min_frame:
+                print("Disabling traffic manager, switching to neural traffic")
+                # for npc in npcs:
+                #     npc.set_autopilot(False)
+                #     control = carla.VehicleControl(steer=0., throttle=0., brake=1.0, hand_brake = False, manual_gear_shift = False)
+                #     npc.apply_control(control)
+                run_nn = True
+
+            if run_nn:
+
+                # (N,channels,rastersize,rastersize)
+                raster = rasterize_input(agents_arr, bb_npcs, list_roads)
+                np.save("testframes/test_"+str(frame_ind),raster[0])
+                print(frame_ind, agents_arr.shape, raster.shape)
+                
+                # Run model
+                raster = torch.tensor(raster, device=device, dtype=torch.float32)
+                confidences, logits  = model(raster)
+                # print(confidences.shape, logits.shape)
+
+                #currpos, yaw = agents_arr[:,-1,:2], agents_arr[:,-1,2]
+                
+                # TO DO improve with array multiplication
+                for j in range(len(agents_arr)):
+
+                    pred = logits[j,confidences[j].argmax()].detach().cpu().numpy()
+
+                    currpos, yaw = agents_arr[j,-1,:2], agents_arr[j,-1,2]*np.pi/180.
+
+                    rot_matrix = np.array([
+                        [np.cos(-yaw), -np.sin(-yaw)],
+                        [np.sin(-yaw), np.cos(-yaw)],
+                    ])
+                
+                    pred = pred@rot_matrix + currpos 
+
+                    if use_nn:
+                        
+                        if use_pid:
+                            # Apply PID controller
+
+                            #target_vel = diffpos/dt
+                            target_vel = (pred[1]-currpos)/(2.*dt)
+                            target_speed = np.sqrt( target_vel[0]**2. + target_vel[1]**2. )
+                            next_wp = get_closest_waypoint(pred[0])
+                            #next_wp = CustomWaypoint(pred[0])
+                            control = controllers[j].run_step(target_speed, next_wp)
+                            npcs[j].apply_control(control)
+
+                        else:
+                            # Displace directly the vehicle to the predicted position
+
+                            nextpos =  pred[0]
+
+                            # Estimate orientation
+                            diffpos = nextpos - currpos
+                            newyaw = np.arctan2(diffpos[1], diffpos[0])*180./np.pi
+                            
+                            nextloc = carla.Location(x=nextpos[0], y=nextpos[1])
+                            nextrot = carla.Rotation(yaw=newyaw)
+
+                            npcs[j].set_transform(carla.Transform(location=nextloc, rotation=nextrot))
+
+                    np.save("testframes/prev_{:d}_{:03d}".format(j, frame_ind),agents_arr[j,:,:2])
+                    np.save("testframes/pred_{:d}_{:03d}".format(j, frame_ind),pred)
+
+            world.tick()
+            frame_ind+=1
+
+    finally:
+        vehicles = world.get_actors().filter('vehicle.*')
+        for vehicle in vehicles:
+            vehicle.destroy()
+        # for npc in npcs:
+        #     npc.destroy()
     
+
+if __name__=="__main__":
+
+    main()
--- roadgraph.py
+++ roadgraph.py
@@ -9,9 +9,15 @@ class RoadGraph():
         self.map = world.get_map()
         self.topology = self.map.get_topology()
         self.each_road_waypoints = self.generate_road_waypoints()
-        self.waypoints = self.map.generate_waypoints(5)
-        self.center = np.array([[waypoint.transform.location.x, waypoint.transform.location.y] for waypoint in self.waypoints]).mean(0)
-          
+        self.center = self.get_center()
+
+
+    def get_center(self):
+        waypoints = self.map.generate_waypoints(5) # Get waypoints just to compute the center
+        center = np.array([[waypoint.transform.location.x, waypoint.transform.location.y] for waypoint in waypoints]).mean(0)
+        return center
+
+
     def generate_road_waypoints(self):
         """Return all, precisely located waypoints from the map.
 
@@ -48,6 +54,29 @@ class RoadGraph():
         return each_road_waypoints
     
 
+    def get_tl_lanes(self, traffic_lights):
+
+        tl_lanes = []
+
+        for tl in traffic_lights:
+
+            affected_lanes_wps = tl.get_affected_lane_waypoints()
+
+            affected_lanes = []
+
+            for wp in affected_lanes_wps:
+                for id, road_wps in enumerate(self.each_road_waypoints):
+                    for waypoint in road_wps:
+                        if waypoint.is_junction:
+                            dist = np.sqrt((waypoint.transform.location.x - wp.transform.location.x)**2. + (waypoint.transform.location.y - wp.transform.location.y)**2.)
+                            if dist<self.precision:
+                                affected_lanes.append(id)
+
+            tl_lanes.append(np.unique(affected_lanes))
+
+        return tl_lanes
+    
+
 if __name__=="__main__":
 
     import carla
@@ -56,23 +85,35 @@ if __name__=="__main__":
 
     raster_size = 224
     displacement = np.array([[raster_size // 2, raster_size // 2]])
+    color_to_rgb = { "Red":(255,0,0), "Yellow":(255,255,0),"Green":(0,255,0) }
 
     client = carla.Client()
     world = client.get_world()
 
-    roadmap = np.ones((raster_size, raster_size, 3), dtype=np.uint8)*256
+    roadmap = np.ones((raster_size, raster_size, 3), dtype=np.uint8)*255
 
     roadnet = RoadGraph(world)
     waypointslist = roadnet.each_road_waypoints
     center = roadnet.center
-    
-    for waypoints in waypointslist:
 
-        road = np.array([[waypoint.transform.location.x, waypoint.transform.location.y] for waypoint in waypoints])
+    traffic_lights = world.get_actors().filter('traffic.traffic_light*')
+
+    tl_lanes = roadnet.get_tl_lanes(traffic_lights)
+    tl_cols = [ tl.get_state() for tl in traffic_lights ]
+    
+    for id, road_wps in enumerate(waypointslist):
+
+        road = np.array([[waypoint.transform.location.x, waypoint.transform.location.y] for waypoint in road_wps])
 
         road = road - center + displacement
 
-        roadmap = cv2.polylines(roadmap,[road.astype(int)],False,(0,0,0))
+        road_color = (0,0,0)
+
+        for j, tl in enumerate(tl_lanes):
+            if id in tl:
+                road_color = color_to_rgb[str(tl_cols[j])]
+
+        roadmap = cv2.polylines(roadmap,[road.astype(int)],False,road_color)
 
     plt.imshow(roadmap)
 
